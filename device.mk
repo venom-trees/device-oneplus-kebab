4bd4f08a68af96b2c1c74aaed5e55976d67e3a01
dataoutputstream <mb150996@gmail.com>
2022-01-07 08:58:09 +0200

    kebab: Build audio.primary.default *Keep quiet log

--- device.mk
+++ device.mk
@@ -132,6 +132,7 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.3-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
+    audio.primary.default \
     audio.primary.kona \
     audio.r_submix.default \
     audio.usb.default \
