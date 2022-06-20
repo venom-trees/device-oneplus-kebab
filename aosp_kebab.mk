c0d0e1b4964bbf9be7564fce902718269864fcbf
VEN0M98 <bt98.bt7@gmail.com>
2022-06-20 23:05:28 +0000

    KEBAB:welcone to VoidUI

--- aosp_kebab.mk
+++ aosp_kebab.mk
@@ -18,17 +18,21 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+
+# Inherit some common VoidUI stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk) 
+WITH_GAPPS := true
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# VoidUI Official Stuff
+CUSTOM_BUILD_TYPE := UNOFFICIAL
+
+# VoidUI Ethernity Maintainer Flags
+VOID_MAINTAINER := VENØM98
+
 # Inherit from kebab device
 $(call inherit-product, device/oneplus/kebab/device.mk)
-
-# Inherit some common Nameless AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-# Boot Animation
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Official
-CUSTOM_BUILD_TYPE := Official
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_kebab
